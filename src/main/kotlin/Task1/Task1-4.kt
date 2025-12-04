940433e11b6644f755c1f5094da056c2b372c049
HOperGT <emilshig5@gmail.com>
2025-12-04 12:57:03 +0300

    Completed variables 1.0

---
+++ src/main/kotlin/Task1/Task1-4.kt
@@ -0,0 +1,17 @@
+package org.example.Task1
+
+fun main(){
+
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    print("Расстояние: $length\n" +
+            "Возраст: $age\n" +
+            "Часть дня: $partOfDay\n" +
+            "Секунды: $seconds\n" +
+            "Часть года: $partOfYear\n" +
+            "Апогей орбиты: $apogee")
+}
