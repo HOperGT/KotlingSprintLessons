79f3cb2544be23f1689c1815c649e43a94930825
HOperGT <emilshig5@gmail.com>
2025-12-04 12:42:07 +0300

    Completed variables 1.0

---
+++ src/main/kotlin/Task1/Task1-3.kt
@@ -0,0 +1,13 @@
+package org.example.Task1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Year: $year\nHour: $hour\nMinute: $minute\n")
+
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
+}
