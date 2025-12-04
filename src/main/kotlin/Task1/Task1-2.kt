a9023635eeada985b9c6053cabb9eba1822c1017
HOperGT <emilshig5@gmail.com>
2025-12-04 11:51:35 +0300

    Completed variables 1.0

---
+++ src/main/kotlin/Task1/Task1-2.kt
@@ -0,0 +1,16 @@
+package org.example.Task1
+
+fun main(){
+    val total: Int = 75
+    var allStaffStore: Int = 2000
+    val thxMess= "Thx for paying this! Total:"
+
+    println("$thxMess $total")
+//    println(AllStaffStore)
+
+    allStaffStore = 1999
+
+    println(allStaffStore)
+
+
+}
