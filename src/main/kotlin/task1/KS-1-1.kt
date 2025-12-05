0e0a410be78a661c3dc3cf025f454fb496cf6aae
HOperGT <emilshig5@gmail.com>
2025-12-05 13:34:12 +0300

    KS-1-1-fixed-output

---
+++ src/main/kotlin/task1/KS-1-1.kt
@@ -0,0 +1,7 @@
+package org.example.task1
+
+fun main(){
+    val total = 75
+
+    val thxMess= "Thx for paying this! Total: $total";
+}
--- src/main/kotlin/task1/Task1.0.kt
+++
@@ -1,9 +0,0 @@
-package org.example.task1
-
-fun main(){
-    var total = 75
-
-    val thxMess= "Thx for paying this! Total:";
-
-    println("$thxMess $total")
-}
